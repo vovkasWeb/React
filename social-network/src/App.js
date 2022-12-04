c9e6a3bae5300c4e7fdc5f75717153ce242742b3
vovkaWeb <vova77557@gmail.com>
2022-12-04 16:06:06 +0200

    update

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -2,44 +2,16 @@
 
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://www.svgrepo.com/show/303340/woocommerce-logo.svg" />
-      </header>
-      <nav className="nav">
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" />
-        </div>
-        <div>ava + description</div>
-        <div>
-          My posts
-          <div>Mew post</div>
-			 <div>
-				<div>post 1</div>
-				<div>post 2</div>
-			 </div>
-        </div>
-      </div>
+      <Header/>
+      <Navbar/>
+		<Profile/>
     </div>
   );
 }
---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = ()=>{
+return(
+	<header className="header">
+	<img src="https://www.svgrepo.com/show/303340/woocommerce-logo.svg" />
+ </header>
+)
+};
+
+export default Header;
