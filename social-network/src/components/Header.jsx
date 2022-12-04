25fee6cdbddd710c00777b10516eff72125f1b20
vovkaWeb <vova77557@gmail.com>
2022-12-04 21:51:38 +0200

    added module

--- social-network/src/components/Header.jsx
+++ social-network/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import s from './Header.module.css';
 
 const Header = ()=>{
 return(
-	<header className="header">
-	<img src="https://www.svgrepo.com/show/303340/woocommerce-logo.svg" />
+	<header className={s.header}>
+	<img src="https://www.svgrepo.com/show/303340/woocommerce-logo.svg"/>
  </header>
 )
 };
--- social-network/src/components/Profile.jsx
+++ social-network/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = ()=>{
 return(
-	<div className="content">
+	<div className={s.content}>
 	<div>
 	  <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" />
 	</div>
@@ -10,9 +11,9 @@ return(
 	<div>
 	  My posts
 	  <div>Mew post</div>
-	  <div>
-		 <div>post 1</div>
-		 <div>post 2</div>
+	  <div className={s.posts}>
+		 <div className={s.item}>post 1</div>
+		 <div className={s.item}>post 2</div>
 	  </div>
 	</div>
  </div>
