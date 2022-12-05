73b05b6f8e669e3e1e3b7c17603ce76d2ea8a2d2
vovkaWeb <vova77557@gmail.com>
2022-12-05 22:39:54 +0200

    added NavLink

--- social-network/src/components/NavBar/Navbar.jsx
+++ social-network/src/components/NavBar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css';
  
 const Navbar = ()=>{
 return(
 	<nav className={s.nav}>
         <div className={s.item}>
-          <a href="/profile">Profile</a>
-        </div>
-        <div className={`${s.item} ${s.active}`}>
-          <a href="/dialogs">Messages</a>
+		  <NavLink to="/profile" className={navData=>navData.isActive? s.activeLink: s.item}> Profile </NavLink>
         </div>
         <div className={s.item}>
-          <a>News</a>
+		  <NavLink to="/dialogs" className={navData=>navData.isActive? s.activeLink: s.item}>Messages</NavLink>
         </div>
         <div className={s.item}>
-          <a>Music</a>
+		  <NavLink to="/news" className={navData=>navData.isActive? s.activeLink: s.item}>News</NavLink>
         </div>
         <div className={s.item}>
-          <a>Settings</a>
+		  <NavLink to="/music" className={navData=>navData.isActive? s.activeLink: s.item}>Music</NavLink>
+        </div>
+        <div className={s.item}>
+		  <NavLink to="/settings" className={navData=>navData.isActive? s.activeLink: s.item}>Settings</NavLink>
         </div>
       </nav>
 )
