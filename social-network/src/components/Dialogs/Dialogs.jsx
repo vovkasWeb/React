2bc5a8a6cc41a58ff7639863c75ea2d04072be5f
vovkaWeb <vova77557@gmail.com>
2022-12-06 11:39:07 +0200

    added text in Dialogs

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css';
 
 const Dialogs =(props)=>{
 return(
-<div >
-    Dialog
+<div className={s.dialogs}>
+   <div className={s.dialogsItems}>
+      <div className={s.dialog + ' ' + s.active}>
+			Vova
+      </div>
+		<div className={s.dialog}>
+			Andrey
+      </div>
+		<div className={s.dialog}>
+			Sasha
+      </div>
+		<div className={s.dialog}>
+			Viktor
+      </div>
+		<div className={s.dialog}>
+			Valera
+      </div>
+	</div>
+	<div className={s.messages}>
+		<div className={s.message}>
+			Hi
+		</div>
+		<div className={s.message}>
+			How are your?
+		</div>
+		<div className={s.message}>
+			Yo
+		</div>
+	</div>
 </div>
 );
 };
