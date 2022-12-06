4d329423442141c9a4ec61a115fbd9d636d2e570
vovkaWeb <vova77557@gmail.com>
2022-12-06 12:49:20 +0200

    update

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <div className="app-wrapper-content">
 		  <Routes>
 				<Route path="/profile" element = {<Profile />}/>
-				<Route path="/dialogs" element = {<Dialogs />}/>
+				<Route path="/dialogs/*" element = {<Dialogs />}/>
 			</Routes>
         </div>
       </div>
