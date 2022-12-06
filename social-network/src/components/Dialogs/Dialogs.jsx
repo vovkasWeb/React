6d89ca559b29518dcd503b9c8b53e53481b4271f
vovkaWeb <vova77557@gmail.com>
2022-12-06 19:57:58 +0200

    update

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -17,20 +17,34 @@ return(
 )
 };
 
+let dialogsData=[
+	{id:1, name:'Vova'},
+	{id:2, name:'Andrey'},
+	{id:3, name:'Sveta'},
+	{id:4, name:'Shasha'},
+	{id:5, name:'Viltor'}
+];
+let messagesData=[
+	{id:1, message:'Hi'},
+	{id:2, message:'How are your?'},
+	{id:3, message:'Yo'},
+	{id:4, message:'Yo'},
+	{id:5, message:'YO'}
+];
+
+
 const Dialogs =(props)=>{
 return(
 <div className={s.dialogs}>
    <div className={s.dialogsItems}>
-     <DialogItem name="Vova" id={1}/>
-	  <DialogItem name="Andrey" id={2}/>
-	  <DialogItem name="Sveta" id={3}/>
-	  <DialogItem name="Shasha" id={4}/>
-	  <DialogItem name="Viktor" id={5}/>
+     <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+	  <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+	 
 	</div>
 	<div className={s.messages}>
-		<Message message="Hi"/>
-		<Message message="How are your?"/>
-		<Message message="Yo"/>
+		<Message message={messagesData[0].message} />
+		<Message message={messagesData[1].message} />
+		<Message message={messagesData[2].message} />
 		
 	</div>
 </div>
--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
+
+let postData=[
+	{id:1, message:'Hi, how are you?',likesCount:12},
+	{id:2, message:'It is my first post',likesCount:11},
+];
+
+
 const MyPosts = ()=>{
 return(
 	
@@ -16,8 +23,8 @@ return(
 		</div>
 		</div>
 	  <div className={s.posts}>
-		 <Post message='Hi, how are you?' likesCount={0}/>
-		 <Post message='It is my first post' likesCount={23}/>
+		 <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+		 <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
 		 
 	  </div>
 	</div>
