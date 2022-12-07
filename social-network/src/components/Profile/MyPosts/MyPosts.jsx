cb06daf463fe72c578d69d70b5b6ddbdf5237e7f
vovkaWeb <vova77557@gmail.com>
2022-12-07 19:51:39 +0200

    update

--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,34 +3,34 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 
-// let posts=[
-// 	{id:1, message:'Hi, how are you?',likesCount:12},
-// 	{id:2, message:'It is my first post',likesCount:11},
-// 	{id:3, message:'Blabla',likesCount:11},
-// 	{id:4, message:'Dada',likesCount:11},
-
-
 const MyPosts = (props)=>{
-	// ];
 let postsElement= props.posts.map(p=><Post message={p.message} likesCount={p.likesCount} key={p.id}/>)
 	
-return(
+let newPostElement = React.createRef();
+
+let addPost=()=>{
+	// let text = newPostElement.current.value;
 	
+	alert(newPostElement.current.value);
+}
+
+
+
+return (
 	<div className={s.postsBlock}>
 	  <h3>My posts</h3>
 	  <div>
 		<div>
-		<textarea></textarea>
+		<textarea ref={newPostElement} ></textarea>
 		</div>
 		<div>
-		<button>add post</button>
+		<button onClick={addPost}>add post</button>
 		</div>
 		</div>
 	  <div className={s.posts}>
 		 {postsElement }
 	  </div>
 	</div>
- 
 )
 };
 
