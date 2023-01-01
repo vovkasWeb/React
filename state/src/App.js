6a6b67564f01e7e09be25348da87374e0c0838a4
vovkaWeb <vova77557@gmail.com>
2023-01-01 20:07:39 +0200

    this

--- state/src/App.js
+++ state/src/App.js
@@ -9,24 +9,44 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 27,
-		text: '+++'
+      text: "+++",
+		position: "",
     };
   }
   nextYear = () => {
     this.setState((state) => ({
       years: state.years + 1,
-		text: state.text +"+"
+      text: state.text + "+",
+      
     }));
   };
+  commitInputChanges = (e,color) =>  {
+	console.log(color);
+		this.setState({
+			position: e.target.value
+		})
+	};
+
   render() {
     const { name, surname, link } = this.props;
+	 const {position,years,text} = this.state;
     return (
       <div>
-        <button onClick={this.nextYear}>{this.state.text}</button>
+        <button onClick={this.nextYear}>{text}</button>
         <h1>
-          My name is {name},surname- {surname}, age-{this.state.years}
+          My name is {name},
+			 surname- {surname}, 
+			 age-{years},
+			  position = {position}
         </h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Введение должность</span>
+          <input
+            type="text"
+            onChange={(e)=>this.commitInputChanges(e,"some color")}
+          />
+        </form>
       </div>
     );
   }
