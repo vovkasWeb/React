4acb99e6070f533c9d81b672ab55f603ed646682
vovkaWeb <vova77557@gmail.com>
2023-01-03 15:49:59 +0200

    styled-components

--- state/src/App.js
+++ state/src/App.js
@@ -1,59 +1,76 @@
 /** @format */
-
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import styled from "styled-components";
 
+const EmpItem = styled.div`
+padding:20px;
+margin-bottom: 15px;
+border-radius: 5px;
+box-shadow: 5px 5px 10px rgba(0,0,0 .2);
+`;
+const Header = styled.h2`
+font-size:22px;
+`;
+export const Button = styled.button`
+display: block;
+padding: 5px 15px;
+bacground-color: gold;
+border:1px solid rgba(0,0,0 .2);
+box-shadow: 5px 5px 10px rgba(0,0,0 .2);
+`;
 class WhoAmI extends Component {
   constructor(props) {
     super(props);
     this.state = {
       years: 27,
       text: "+++",
-		position: "",
+      position: "",
     };
   }
   nextYear = () => {
     this.setState((state) => ({
       years: state.years + 1,
       text: state.text + "+",
-      
     }));
   };
-  commitInputChanges = (e,color) =>  {
-	console.log(color);
-		this.setState({
-			position: e.target.value
-		})
-	};
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value,
+    });
+  };
 
   render() {
     const { name, surname, link } = this.props;
-	 const {position,years,text} = this.state;
+    const { position, years, text } = this.state;
     return (
-      <>
-        <button onClick={this.nextYear}>{text}</button>
-        <h1>
-          My name is {name},
-			 surname- {surname}, 
-			 age-{years},
-			  position = {position}
-        </h1>
+      <EmpItem >
+        <Button onClick={this.nextYear}>{text}</Button>
+        <Header>
+          My name is {name}, surname- {surname}, age-{years}, position ={" "}
+          {position}
+        </Header >
         <a href={link}>My profile</a>
         <form>
           <span>Введение должность</span>
           <input
             type="text"
-            onChange={(e)=>this.commitInputChanges(e,"some color")}
+            onChange={(e) => this.commitInputChanges(e, "some color")}
           />
         </form>
-      </>
+      </EmpItem >
     );
   }
 }
+const Wrapper= styled.div`
+width:600px;
+margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+   <Wrapper>
       <WhoAmI
         name="John"
         surname="Smith"
@@ -64,7 +81,7 @@ function App() {
         surname="Smith"
         link="Vk.com"
       />
-    </div>
+    </Wrapper>
   );
 }
 
--- state/src/index.js
+++ state/src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** @format */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Button } from "./App";
+import styled from "styled-components";
+
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+`;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <BigButton as="a">+++</BigButton>
   </React.StrictMode>
 );
 
